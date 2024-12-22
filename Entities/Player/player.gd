aaef9654210677abe0039437b5244aea268b12ee
nohiro <nyan.gos4@gmail.com>
2024-12-23 01:36:37 +0900

    1-8: call animations

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -6,6 +6,8 @@ const friction: int = 400
 var input: Vector2 = Vector2.ZERO
 
 @export var speed: int = 70
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
 
 func _process(delta: float) -> void:
 	player_movement(delta)
@@ -21,13 +23,21 @@ func player_movement(delta: float) -> void:
 	input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if input != Vector2.ZERO:
+		set_animation_state()
+		animation_state.travel("Move")
 		velocity = velocity.limit_length(speed)
 	
 	if input == Vector2.ZERO:
 		if velocity.length() > (friction * delta):
 			velocity -= velocity.normalized() * (friction * delta)
 		else:
+			animation_state.travel("Idle")
 			velocity = Vector2.ZERO
 	
 	velocity += diagonal_movement(input * acceleration * delta)
 	move_and_slide()
+
+func set_animation_state() -> void:
+	animation_tree.set("parameters/Idle/blend_position", input)
+	animation_tree.set("parameters/Move/blend_position", input)
+	animation_tree.set("parameters/Attack/blend_position", input)
