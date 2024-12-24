decb75b709a0a3b1a18e6c9e6d1e167050c1ab63
nohiro <nyan.gos4@gmail.com>
2024-12-25 04:07:53 +0900

    1-16: update damage

--- Entities/Player/player_data.gd
+++ Entities/Player/player_data.gd
@@ -1,4 +1,7 @@
 class_name PlayerData
 extends Node
 
-static var health: int = 12
+static var health: float = 12
+
+static func update_player_health(amount: float) -> void:
+	health += amount
--- Interactables/Hazards/spike_trap.gd
+++ Interactables/Hazards/spike_trap.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
-
+@export var damage: float = -0.25
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		PlayerData.health -= 1
+		PlayerData.update_player_health(damage)
+		print(PlayerData.health)
