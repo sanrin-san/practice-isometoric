95f78faf42c782ff2afeef0431991b9e5c690488
nohiro <nyan.gos4@gmail.com>
2024-12-25 03:38:16 +0900

    1-14: create player health

---
+++ Entities/Player/player_data.gd
@@ -0,0 +1,4 @@
+class_name PlayerData
+extends Node
+
+static var health: int = 12
---
+++ UI/gui.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+@onready var heart: Sprite2D = $Heart
+
+const heart_row_size: int = 8
+const heart_offset: int = 16
+
+func _ready() -> void:
+	for i in PlayerData.health:
+		var new_heart: Sprite2D = Sprite2D.new()
+		new_heart.texture = heart.texture
+		new_heart.hframes = heart.hframes
+		heart.add_child(new_heart)
+
+func _process(delta: float) -> void:
+	for heart_child in heart.get_children():
+		var index = heart_child.get_index()
+		var x = (index % heart_row_size) * heart_offset
+		var y = (index / heart_row_size) * heart_offset
+		heart_child.position = Vector2(x, y)
+		
+		var last_heart = floor(PlayerData.health)
+		if index > last_heart:
+			heart_child.frame = 0
+		if index == last_heart:
+			heart_child.frame = (PlayerData.health - last_heart) * 4
+		if index < last_heart:
+			heart_child.frame = 4
