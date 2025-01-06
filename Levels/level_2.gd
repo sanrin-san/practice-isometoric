760681028db3c46be61cadc8d934575f3bcfdb6a
nohiro <nyan.gos4@gmail.com>
2025-01-07 04:23:30 +0900

    update: dialogic

--- Levels/level_2.gd
+++ Levels/level_2.gd
@@ -6,10 +6,8 @@ var is_amina_intaract: bool = false
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_interact"):
 		if is_amina_intaract == true:
-			print("a")
 			if Dialogic.current_timeline != null:
 				return
-			print("b")
 			Dialogic.start('test')
 
 func _on_intaract_area_entered(area: Area2D) -> void:
