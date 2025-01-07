a99ac4548284293b5c4eccbbeb484a232252421f
nohiro <nyan.gos4@gmail.com>
2025-01-08 05:17:21 +0900

    update: amina portrait

--- Levels/level_2.gd
+++ Levels/level_2.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 var is_amina_intaract: bool = false
 
-
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_interact"):
 		if is_amina_intaract == true:
 			if Dialogic.current_timeline != null:
 				return
+			Dialogic.VAR.amina.cloth = "one-piece"
 			Dialogic.start('test')
 
 func _on_intaract_area_entered(area: Area2D) -> void:
